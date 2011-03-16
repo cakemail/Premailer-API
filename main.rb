1eb17bfd2d6050d992317916a8e93297524828bc
Christian Joudrey <cmallette@gmail.com>
2011-03-15 20:49:54 -0400

    Initial Commit

---
+++ main.rb
@@ -0,0 +1,29 @@
+require 'sinatra'
+require 'premailer'
+require 'json'
+
+configure :production do
+  set :port, 80
+end
+
+get '/premailer' do
+<<-eos
+  <form method="POST">
+  <textarea name="html"></textarea>
+  <button>Submit</button>
+  <input type="hidden" name="with_warnings" value="1"/>
+  </form>
+eos
+end
+
+post '/premailer' do
+  with_warnings = params[:with_warnings] == '1' ? true : false
+  html = params[:html] ? params[:html] : request.body.read
+
+  premailer = Premailer.new(html, :warn_level => Premailer::Warnings::SAFE, :with_html_string => true)
+
+  content_type :json
+  data = {:html => premailer.to_inline_css}
+  data[:warnings] = premailer.warnings if with_warnings
+  data.to_json
+end
