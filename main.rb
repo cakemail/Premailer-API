7bb22368c3424f9d8f6cd7af5b5cffd84a64793a
Christian Joudrey <cmallette@gmail.com>
2012-01-02 14:05:05 -0500

    Added "remove_warnings"

--- main.rb
+++ main.rb
@@ -20,9 +20,15 @@ post '/clean' do
   error 400 if !params[:html]
 
   with_warnings = params[:with_warnings] == '1' ? true : false
+  remove_comments = params[:remove_comments] == '1' ? true : false
   html = params[:html]
 
-  premailer = Premailer.new(html, :warn_level => Premailer::Warnings::SAFE, :with_html_string => true, :preserve_styles => true)
+  premailer = Premailer.new(html,
+    :warn_level => Premailer::Warnings::SAFE,
+    :with_html_string => true,
+    :preserve_styles => true,
+    :remove_comments => remove_comments
+  )
 
   content_type :json
   data = {:html => premailer.to_inline_css}
